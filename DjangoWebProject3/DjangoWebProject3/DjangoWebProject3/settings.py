093098b49f5a83db0c06627e52a9d60bb28503ed
bülent <121733911+bulent-trn@users.noreply.github.com>
2023-02-05 15:28:08 +0300

    temco deneme

--- DjangoWebProject3/DjangoWebProject3/DjangoWebProject3/settings.py
+++ DjangoWebProject3/DjangoWebProject3/DjangoWebProject3/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '31ffd645-5009-4513-8746-43370649f9e7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # 'temco-blog-app.azurewebsites.net'
-ALLOWED_HOSTS = ["https://temcoo.azurewebsites.net"]
+ALLOWED_HOSTS = ["temcoo.azurewebsites.net"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
